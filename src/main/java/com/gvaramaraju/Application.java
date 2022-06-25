94f64279f1b9b6c352728cdb6b3c6fa12cb646fa
gvaramaraju <ramaraju.gva@gmail.com>
2022-06-25 10:58:18 +0530

    Initial commit

--- src/main/java/com/gvaramaraju/Application.java
+++ src/main/java/com/gvaramaraju/Application.java
@@ -2,8 +2,18 @@ package com.gvaramaraju;
 
 import io.micronaut.runtime.Micronaut;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+
 public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
+    }
+
+    @Path("/greet")
+    @GET
+    public String greet(){
+        return "Hello World!!";
     }
 }
